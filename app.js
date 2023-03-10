a9fbade7208167f962dd4b1e2c252736f7b3ded0
AhmaDev <ahmadwig@gmail.com>
2023-03-10 21:39:06 +0300

    fix

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors());
